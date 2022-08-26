2c2331f29cd8cf82123afb04c64464db6accab0d
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2022-08-26 16:28:21 +0900

    [Bronze III] Title: 네 번째 점, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,13 @@
+arrx=[]
+arry=[]
+for i in range(3):
+    x,y=map(int,input().split())
+    if x in arrx:
+        arrx.remove(x)
+    else:
+        arrx.append(x)
+    if y in arry:
+        arry.remove(y)
+    else:
+        arry.append(y)
+print(arrx[0], arry[0])
