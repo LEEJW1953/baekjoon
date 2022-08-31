2e7e0ce5631d8de71c894d43903fed49f30a6b16
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2022-08-31 15:42:30 +0900

    [Silver II] Title: 조합 0의 개수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
@@ -1,63 +1,20 @@
 import sys
 input=sys.stdin.readline
 
+def com(x, y):
+    count=0
+    i=1
+    while True:
+        a=y**i
+        if x//a==0:
+            break
+        else:
+            count+=x//a
+            i+=1
+    return count
+
 n, m=map(int,input().split())
 r=n-m
-count1=0
-count2=0
-count3=0
-count4=0
-count5=0
-count6=0
-i=1
-while True:
-    a=5**i
-    if n//a==0:
-        i=1
-        break
-    else:
-        count1+=n//a
-        i+=1
-while True:
-    a=5**i
-    if m//a==0:
-        i=1
-        break
-    else:
-        count2+=m//a
-        i+=1
-while True:
-    a=5**i
-    if r//a==0:
-        i=1
-        break
-    else:
-        count3+=r//a
-        i+=1
-while True:
-    a=2**i
-    if n//a==0:
-        i=1
-        break
-    else:
-        count4+=n//a
-        i+=1
-while True:
-    a=2**i
-    if m//a==0:
-        i=1
-        break
-    else:
-        count5+=m//a
-        i+=1
-while True:
-    a=2**i
-    if r//a==0:
-        i=1
-        break
-    else:
-        count6+=r//a
-        i+=1
-result1=count1-count2-count3
-result2=count4-count5-count6
+result1=com(n,5)-com(m,5)-com(r,5)
+result2=com(n,2)-com(m,2)-com(r,2)
 print(min(result1, result2))
