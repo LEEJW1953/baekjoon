5cb6e47b8b659dce8c0c84dfb15bb0efb0d2c87c
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2022-10-18 19:46:01 +0900

    [Silver IV] Title: 수 찾기, Time: 648 ms, Memory: 47432 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,26 @@
+from re import L
+import sys
+input=sys.stdin.readline
+
+def fun(a):
+    start=0
+    end=n-1
+    result=0
+    while start<=end:
+        mid=(start+end)//2
+        if arr[mid]==a:
+            result=1
+            return result
+        if arr[mid]<a:
+            start=mid+1
+        elif a<arr[mid]:
+            end=mid-1
+    return result
+
+n=int(input())
+arr=list(map(int, input().split()))
+arr.sort()
+m=int(input())
+arr1=list(map(int, input().split()))
+for i in range(m):
+    print(fun(arr1[i]))
