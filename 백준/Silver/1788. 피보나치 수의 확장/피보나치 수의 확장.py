eea5a5d6ab2678e1b5920d98db0bfcedaa5ed42c
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2022-12-09 21:27:19 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 324 ms, Memory: 70452 KB -BaekjoonHub

--- 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
@@ -2,25 +2,13 @@ import sys
 input=sys.stdin.readline
 
 n=int(input())
-if n>=0:
-    arr=[0, 1]
-    for i in range(2, n+1):
-        arr.append((arr[i-2]+arr[i-1])%1000000000)
-elif n<0:
-    arr=[1, 0, 1]
-    for i in range(2, -n+1):
-        if arr[i-1]-arr[i]<0:
-            arr.append(-((-arr[i-1]+arr[i])%1000000000))
-        else:
-            arr.append((arr[i-1]-arr[i])%1000000000)
-if n==0:
-    print(0)
+arr=[0, 1]
+for i in range(2, abs(n)+1):
+    arr.append((arr[i-2]+arr[i-1])%1000000000)
+if n%2==0 and n<0:
+    print(-1)
+elif n==0:
     print(0)
 else:
-    if arr[-1]>0:
-        print(1)
-    elif arr[-1]==0:
-        print(0)
-    else:
-        print(-1)
-    print(abs(arr[-1]))
+    print(1)
+print(arr[abs(n)])
