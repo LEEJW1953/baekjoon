e5339c23a58bf430375a9279f47e8e32339319a6
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2022-12-26 18:15:53 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 64 ms, Memory: 30952 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,37 @@
+import sys
+input=sys.stdin.readline
+
+def dfs(i, num):
+    global arr, op, minval, maxval
+    if i==n:
+        maxval=max(maxval, num)
+        minval=min(minval, num)
+        res.append(maxval)
+        res.append(minval)
+    else:
+        if op[0]>0:
+            op[0]-=1
+            dfs(i+1, num+arr[i])
+            op[0]+=1
+        if op[1]>0:
+            op[1]-=1
+            dfs(i+1, num-arr[i])
+            op[1]+=1
+        if op[2]>0:
+            op[2]-=1
+            dfs(i+1, num*arr[i])
+            op[2]+=1
+        if op[3]>0:
+            op[3]-=1
+            dfs(i+1, int(num/arr[i]))
+            op[3]+=1
+
+n=int(input())
+arr=list(map(int, input().split()))
+op=list(map(int, input().split()))
+minval=1e9
+maxval=-1e9
+res=[]
+dfs(1, arr[0])
+print(maxval)
+print(minval)
