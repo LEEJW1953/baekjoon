27deaa5177d0b3f859461d7467c00cabea5e6496
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-01-06 16:26:24 +0900

    [Gold V] Title: 동전, Time: 72 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    coin=list(map(int, input().split()))
+    p=int(input())
+    dp=[0]*(p+1)
+    dp[0]=1
+    for i in coin:
+        for j in range(i, p+1):
+            if i<=j:
+                dp[j]+=dp[j-i]
+    print(dp[p])
