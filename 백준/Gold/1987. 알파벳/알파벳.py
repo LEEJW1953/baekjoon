90c20a9c653007330e784ff01ddc15db41612598
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-01-09 21:14:24 +0900

    [Gold IV] Title: 알파벳, Time: 1332 ms, Memory: 51996 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,22 +1,25 @@
-r, c = map(int, input().split())
-arr = []
-for _ in range(r):
-    arr.append(list(input()))
-ans = 0
-alphas = set()
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+import sys
+input=sys.stdin.readline
 
-def dfs(x, y, count):
-    global ans
-    ans = max(ans, count)
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < r and 0 <= ny < c and not arr[nx][ny] in alphas:
-            alphas.add(arr[nx][ny])
-            dfs(nx, ny, count+1)
-            alphas.remove(arr[nx][ny])
-alphas.add(arr[0][0])
-dfs(0, 0, 1)
-print(ans)
+# bfs
+def bfs():
+    global cnt
+    queue = set([(0, 0, graph[0][0])])
+    while queue:
+        x, y, z = queue.pop()
+        cnt = max(cnt, len(z))
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < r and 0 <= ny < c and graph[nx][ny] not in z:
+                queue.add((nx, ny, graph[nx][ny] + z))
+
+
+r, c = map(int, input().split())
+graph = [list(map(str, input().strip())) for _ in range(r)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+cnt = 1
+bfs()
+print(cnt)
