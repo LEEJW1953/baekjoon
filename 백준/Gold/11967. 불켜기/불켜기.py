88dcbb45b76c58748869605e533f7d6f77a1819f
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-01-12 20:57:07 +0900

    [Gold II] Title: 불켜기, Time: 108 ms, Memory: 35816 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.py
+++ 백준/Gold/11967. 불켜기/불켜기.py
@@ -2,23 +2,14 @@ import sys
 input=sys.stdin.readline
 from collections import deque
 
-def bfs(a, b):
-    q=deque()
-    arr2=[]
-    visit=[[0]*n for _ in range(n)]
-    q.append([a, b])
-    while q:
-        [x, y]=q.popleft()
-        for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            if 0<=nx<n and 0<=ny<n:
-                if arr[ny][nx] and not visit[ny][nx]:
-                    if not vis[ny][nx]:
-                        arr2.append([nx, ny])
-                    q.append([nx, ny])
-                    visit[ny][nx]=1
-    return arr2
+def bfs1(x, y):
+    arr3=[]
+    for i in range(4):
+        nx=x+dx[i]
+        ny=y+dy[i]
+        if 0<=nx<n and 0<=ny<n and vis[ny][nx]:
+            arr3.append([nx, ny])
+    return arr3
 
 def switch():
     q=deque()
@@ -26,14 +17,21 @@ def switch():
     arr[0][0]=1
     while q:
         [x, y]=q.popleft()
-        for i in range(len(s[y][x])):
-            [nx, ny]=s[y][x][i]
-            arr[ny][nx]=1
-        arr1=bfs(x, y)
-        if arr1:
-            for i in arr1:
-                q.append(i)
-                vis[i[1]][i[0]]=1
+        for i in s[y][x]:
+            [nx, ny]=i
+            if not arr[ny][nx]:
+                arr[ny][nx]=1
+                arr1=bfs1(nx, ny)
+                if arr1:
+                    for i in arr1:
+                        q.append(i)
+                        vis[i[1]][i[0]]=1
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if 0<=nx<n and 0<=ny<n and arr[ny][nx] and not vis[ny][nx]:
+                q.append([nx, ny])
+                vis[ny][nx]=1
 
 dx=[1, -1, 0, 0]
 dy=[0, 0, 1, -1]
