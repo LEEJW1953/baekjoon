12001f285d3ec368c9b0056acecbbb7ccfcae539
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-01-13 19:38:08 +0900

    [Silver II] Title: N과 M (11), Time: 728 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -2,25 +2,19 @@ import sys
 input=sys.stdin.readline
 
 def f():
+    q=0
     if len(s)==m:
-        # print(*s)
-        ans.add(' '.join(map(str, s)))
+        print(*s)
         return
     for i in range(n):
-        s.append(arr[i])
-        f()
-        s.pop()
+        if arr[i]!=q:
+            s.append(arr[i])
+            q=arr[i]
+            f()
+            s.pop()
 
 n, m=map(int, input().split())
 arr=list(map(int, input().split()))
 arr.sort()
 s=[]
-ans=set()
 f()
-ans=list(ans)
-res=[]
-for i in range(len(ans)):
-    res.append(list(map(int, ans[i].split())))
-res.sort()
-for i in range(len(res)):
-    print(*res[i])
