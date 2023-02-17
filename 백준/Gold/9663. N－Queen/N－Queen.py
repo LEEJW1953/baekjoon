4c2ac1fb82e8a9e459f26d121cd176fd83e5ad88
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-02-17 21:36:37 +0900

    [Gold IV] Title: N-Queen, Time: 26216 ms, Memory: 204440 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,25 +1,25 @@
 import sys
 input=sys.stdin.readline
 
+def f():
+    global count
+    if len(arr)==n:
+        count+=1
+        return
+    for i in range(n):
+        if check(i):
+            arr.append(i)
+            f()
+            arr.pop()
+
 def check(x):
-    for i in range(x):
-        if arr[i]==arr[x] or abs((arr[i]-arr[x]))==abs(i-x):
+    for i in range(len(arr)):
+        if arr[i]==x or ((i+arr[i])==(len(arr)+x)) or ((i-arr[i])==(len(arr)-x)):
             return False
     return True
 
-def queen(x):
-    global count
-    if x==n:
-        count+=1
-    else:
-        for i in range(n):
-            arr[x]=i
-            if check(x):
-                queen(x+1)
-
-
 n=int(input())
-arr=[0]*n
 count=0
-queen(0)
+arr=[]
+f()
 print(count)
