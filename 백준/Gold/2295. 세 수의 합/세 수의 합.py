305da6064a3d54396818b4c2aa1807fe51ea761a
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-03-18 22:11:36 +0900

    [Gold IV] Title: 세 수의 합, Time: 576 ms, Memory: 66076 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -1,36 +1,18 @@
 import sys
 input=sys.stdin.readline
 
-def binary(x, st, en):
-    mid=(st+en)//2
-    if en-st==1 or en==st:
-        if x==two[mid]:
-            return True
-        else:
-            return False
-    elif x==two[mid]:
-        return True
-    elif x<two[mid]:
-        return binary(x, st, mid)
-    elif x>two[mid]:
-        return binary(x, mid, en)
-    return
-
 n=int(input())
 u=[]
-answer=0
+answer={}
 for i in range(n):
     u.append(int(input()))
 u=list(set(u))
-u.sort()
-two=[]
+two=set()
 for i in range(n):
     for j in range(n):
-        two.append(u[i]+u[j])
-two=list(set(two))
-two.sort()
+        two.add(u[i]+u[j])
 for i in range(len(u)):
-    for j in range(i+1):
-        if binary((u[i]-u[j]), 0, len(two)):
-            answer=max(answer, u[i])
-print(answer)
+    for j in range(len(u)):
+        if (u[i]-u[j]) in two:
+            answer[u[i]]=(u[i], u[j], u[i]-u[j])
+print(sorted(list(answer.keys()))[-1])
