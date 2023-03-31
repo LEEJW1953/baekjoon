22825812f7eb1146149d291f5c78723878496f6d
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-03-31 22:34:35 +0900

    [Gold V] Title: 인구 이동, Time: 5804 ms, Memory: 34364 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -2,15 +2,6 @@ import sys
 input=sys.stdin.readline
 from collections import deque
 
-def f():
-    for i in range(n):
-        for j in range(n):
-            for k in range(4):
-                nx, ny = i+dx[k], j+dy[k]
-                if 0<=nx<n and 0<=ny<n and l<=g[nx][ny]-g[i][j]<=r:
-                    return False
-    return True
-
 def bfs(x, y):
     q=deque()
     q.append([x, y])
@@ -29,7 +20,10 @@ def bfs(x, y):
                     count+=1
                     vis[nx][ny]=1
                     q.append([nx, ny])
-    return [arr, total, count]
+    if len(arr)!=1:
+        return [arr, total, count]
+    else:
+        return []
 
 n, l, r = map(int, input().split())
 g=[list(map(int, input().split())) for _ in range(n)]
@@ -37,14 +31,16 @@ dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 answer=0
 while True:
-    if f():
-        break
     vis=[[0]*n for _ in range(n)]
     tmp=[]
     for i in range(n):
         for j in range(n):
             if not vis[i][j]:
-                tmp.append(bfs(i, j))
+                tmp1=bfs(i, j)
+                if tmp1:
+                    tmp.append(tmp1)
+    if not tmp:
+        break
     for i in range(len(tmp)):
         val=tmp[i][1]//tmp[i][2]
         for j in tmp[i][0]:
