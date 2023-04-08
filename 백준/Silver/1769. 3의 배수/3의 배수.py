c5e1591e5c38eb9e21b28689e2562655bb0dcecf
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-04-08 21:47:50 +0900

    [Silver V] Title: 3의 배수, Time: 156 ms, Memory: 41408 KB -BaekjoonHub

--- 백준/Silver/1769. 3의 배수/3의 배수.py
+++ 백준/Silver/1769. 3의 배수/3의 배수.py
@@ -1,13 +1,13 @@
 import sys
 input=sys.stdin.readline
 
-n=input().strip()
-count=0
-while 10<=int(n):
-    n=str(sum(list(map(int, n))))
-    count+=1
-print(count)
-if int(n)%3:
+n=list(map(int, input().strip()))
+c=0
+while len(n)>1:
+    n=list(map(int, str(sum(n))))
+    c+=1
+print(c)
+if sum(n)%3:
     print('NO')
 else:
     print('YES')
