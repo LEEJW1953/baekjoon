ee5dc697f02f33ca865b373555dd44f5ad4c0e5e
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-04-10 23:01:51 +0900

    [Gold V] Title: 보물섬, Time: 3024 ms, Memory: 180540 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -7,17 +7,15 @@ def bfs(x, y):
     q.append((x, y))
     vis={}
     vis[(x, y)]=0
-    res=0
     while q:
         xx, yy = q.popleft()
-        res = max(res, vis[(xx, yy)])
         for i in range(4):
             nx, ny = xx+dx[i], yy+dy[i]
             if 0<=nx<n and 0<=ny<m:
                 if g[nx][ny]=='L' and (nx, ny) not in vis.keys():
                     vis[(nx, ny)]=vis[(xx, yy)]+1
                     q.append([nx, ny])
-    return res
+    return max(vis.values())
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
