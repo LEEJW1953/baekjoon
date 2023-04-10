3390a81536c73ed0f2f32ca5a1760aac57b743fa
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-04-10 22:56:02 +0900

    [Gold V] Title: 보물섬, Time: 3676 ms, Memory: 197228 KB -BaekjoonHub

---
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -0,0 +1,31 @@
+import sys
+input=sys.stdin.readline
+from collections import deque
+
+def bfs(x, y):
+    q=deque()
+    q.append((x, y))
+    vis={}
+    vis[(x, y)]=0
+    res=0
+    while q:
+        xx, yy = q.popleft()
+        res = max(res, vis[(xx, yy)])
+        for i in range(4):
+            nx, ny = xx+dx[i], yy+dy[i]
+            if 0<=nx<n and 0<=ny<m:
+                if g[nx][ny]=='L' and (nx, ny) not in vis.keys():
+                    vis[(nx, ny)]=vis[(xx, yy)]+1
+                    q.append([nx, ny])
+    return res
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+n, m = map(int, input().split())
+g=[list(input().strip()) for _ in range(n)]
+ans=0
+for i in range(n):
+    for j in range(m):
+        if g[i][j]=='L':
+            ans = max(ans, bfs(i, j))
+print(ans)
