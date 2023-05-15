7c4c08ae231401f4b3a45f05e1b71b740323c6a1
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-05-15 23:56:49 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 696 ms, Memory: 57636 KB -BaekjoonHub

---
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
@@ -0,0 +1,22 @@
+import sys
+input=sys.stdin.readline
+
+def search(target):
+    s, e=0, n-1
+    while s<=e:
+        mid=(s+e)//2
+        if target<=int(stat[mid][1]):
+            e=mid-1
+        else:
+            s=mid+1
+    return e+1
+
+n, m=map(int, input().split())
+stat=[list(map(str, input().split())) for _ in range(n)]
+for _ in range(m):
+    c=int(input())
+    print(stat[search(c)][0])
+    # for i in range(n):
+    #     if c<=int(stat[i][1]):
+    #         print(stat[i][0])
+    #         break
