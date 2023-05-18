92a85421a23f9ef7d434be7e4679e66357d4b39d
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-05-18 23:57:23 +0900

    [Gold V] Title: 도로의 개수, Time: 204 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1577. 도로의 개수/도로의 개수.py
@@ -0,0 +1,53 @@
+import sys
+input=sys.stdin.readline
+
+n, m = map(int, input().split())
+k=int(input())
+res=[[0]*(m+1) for _ in range(n+1)]
+res[0][0]=1
+arr=[list(map(int, input().split())) for _ in range(k)]
+
+for i in range(1, n+1):
+    x, nx = i-1, i
+    tmp=True
+    for l in range(k):
+        a=min(arr[l][0], arr[l][2])
+        b=min(arr[l][1], arr[l][3])
+        c=max(arr[l][0], arr[l][2])
+        d=max(arr[l][1], arr[l][3])
+        if (x, 0, nx, 0)==(a, b, c, d):
+            tmp=False
+    if tmp:
+        res[i][0]+=res[i-1][0]
+
+for i in range(1, m+1):
+    y, ny = i-1, i
+    tmp=True
+    for l in range(k):
+        a=min(arr[l][0], arr[l][2])
+        b=min(arr[l][1], arr[l][3])
+        c=max(arr[l][0], arr[l][2])
+        d=max(arr[l][1], arr[l][3])
+        if (0, y, 0, ny)==(a, b, c, d):
+            tmp=False
+    if tmp:
+        res[0][i]+=res[0][i-1]
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        x1, y1, x2, y2, nx, ny = i-1, j, i, j-1, i, j
+        tmp1, tmp2 = True, True
+        for l in range(k):
+            a=min(arr[l][0], arr[l][2])
+            b=min(arr[l][1], arr[l][3])
+            c=max(arr[l][0], arr[l][2])
+            d=max(arr[l][1], arr[l][3])
+            if (x1, y1, nx, ny)==(a, b, c, d):
+                tmp1=False
+            if (x2, y2, nx, ny)==(a, b, c, d):
+                tmp2=False
+        if tmp1:
+            res[nx][ny]+=res[x1][y1]
+        if tmp2:
+            res[nx][ny]+=res[x2][y2]
+print(res[-1][-1])
