b0368c95f16ca24cf174fc17335c66b790b87fc2
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-05-20 02:55:23 +0900

    [Gold IV] Title: 무기 공학, Time: 7392 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/18430. 무기 공학/무기 공학.py
@@ -0,0 +1,40 @@
+import sys
+input=sys.stdin.readline
+
+def boom(x, y, nx1, nx2, ny1, ny2):
+    if 0<=nx1<n and 0<=nx2<n and 0<=ny1<m and 0<=ny2<m and not vis[x][y] and not vis[nx1][ny1] and not vis[nx2][ny2]:
+        return wood[x][y]*2+wood[nx1][ny1]+wood[nx2][ny2]
+    return 0
+
+def boomerang(x, y, total, num):
+    global ans
+    ans=max(ans, total)
+    for i in range(n):
+        for j in range(m):
+            if i<x:
+                continue
+            elif i==x and j<y:
+                continue
+            else:
+                for k in range(4):
+                    nx1, ny1, nx2, ny2 = i+new[k][0], j+new[k][1], i+new[k][2], j+new[k][3]
+                    t=boom(i, j, nx1, nx2, ny1, ny2)
+                    if t:
+                        vis[i][j]=1
+                        vis[nx1][ny1]=1
+                        vis[nx2][ny2]=1
+                        boomerang(i, j, total+t, num+1)
+                        vis[i][j]=0
+                        vis[nx1][ny1]=0
+                        vis[nx2][ny2]=0
+    return
+
+new=[[1, 0, 0, -1], [-1, 0, 0, -1], [-1, 0, 0, 1], [1, 0, 0, 1]]
+n, m = map(int, input().split())
+wood=[list(map(int, input().split())) for _ in range(n)]
+vis=[[0]*m for _ in range(n)]
+ans=0
+for i in range(n):
+    for j in range(m):
+        boomerang(i, j, 0, 1)
+print(ans)
