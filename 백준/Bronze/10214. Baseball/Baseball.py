5b47b383bb08388dd9fa6eb4cb91b8cfc556f042
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-06-15 22:20:11 +0900

    [Bronze III] Title: Baseball, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10214. Baseball/Baseball.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+for _ in range(t):
+    y, k = 0, 0
+    for i in range(9):
+        a, b = map(int, input().split())
+        y+=a
+        k+=b
+    if y>k:
+        print('Yonsei')
+    elif y<k:
+        print('Korea')
+    else:
+        print('Draw')
