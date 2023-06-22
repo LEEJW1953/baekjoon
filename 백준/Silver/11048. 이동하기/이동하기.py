67273c5d5788427e831f4dbef4350b3249ee9e77
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-06-22 23:59:23 +0900

    [Silver II] Title: 이동하기, Time: 2468 ms, Memory: 78676 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -4,12 +4,12 @@ input=sys.stdin.readline
 n, m = map(int, input().split())
 g=[list(map(int, input().split())) for _ in range(n)]
 move=[[0, 1], [1, 0], [1, 1]]
-dp=[[[] for _ in range(m)] for _ in range(n)]
-dp[0][0].append(g[0][0])
+dp=[[0]*m for _ in range(n)]
+dp[0][0]=g[0][0]
 for i in range(n):
     for j in range(m):
         for k in range(3):
             nx, ny = i+move[k][0], j+move[k][1]
             if 0<=nx<n and 0<=ny<m:
-                dp[nx][ny].append(max(dp[i][j])+g[nx][ny])
-print(max(dp[-1][-1]))
+                dp[nx][ny]=max(dp[i][j]+g[nx][ny],dp[nx][ny])
+print(dp[-1][-1])
