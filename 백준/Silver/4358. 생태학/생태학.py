bcabbe72b0b1205e70148336219ca58bb2d7e372
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-06-24 04:25:24 +0900

    [Silver II] Title: 생태학, Time: 484 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.py
+++ 백준/Silver/4358. 생태학/생태학.py
@@ -2,17 +2,15 @@ import sys
 input=sys.stdin.readline
 from collections import defaultdict
 
-arr=[]
 d=defaultdict(int)
 total=0
 while True:
     s=input().strip()
     if not s:
         break
-    if s not in arr:
-        arr.append(s)
     total+=1
     d[s]+=1
+arr=list(d.items())
 arr.sort()
 for i in range(len(arr)):
-    print(arr[i], '{:.4f}'.format(d[arr[i]]/total*100))
+    print(arr[i][0], '{:.4f}'.format(arr[i][1]/total*100))
