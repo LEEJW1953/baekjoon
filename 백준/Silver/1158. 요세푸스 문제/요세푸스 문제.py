d1ec65ad79986271a9735a0e9385a55970598945
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-09-14 04:48:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 68 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -5,8 +5,7 @@ from collections import deque
 n, k = map(int, input().split())
 q=deque(range(1, n+1))
 arr=[]
-for i in range(n):
-    for j in range(k):
-        q.append(q.popleft())
-    arr.append(str(q.pop()))
+while q:
+    q.rotate(-k+1)
+    arr.append(str(q.popleft()))
 print(f'<{", ".join(arr)}>')
