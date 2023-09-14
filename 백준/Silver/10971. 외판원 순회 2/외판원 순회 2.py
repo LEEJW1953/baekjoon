43cc2ad29dab529f495531ecfcefa4711c261e47
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-09-15 02:38:49 +0900

    [Silver II] Title: 외판원 순회 2, Time: 2200 ms, Memory: 119368 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -0,0 +1,27 @@
+import sys
+input=sys.stdin.readline
+
+def f(s, x, d, count):
+    global ans
+    if 0 not in vis:
+        ans=min(ans, d)
+        return
+    for i in range(n):
+        if g[x][i]==0:
+            continue
+        if count!=n-1 and i==s:
+            continue
+        if not vis[i]:
+            vis[i]=g[x][i]
+            f(s, i, d+g[x][i], count+1)
+            vis[i]=0
+
+n=int(input())
+g=[]
+vis=[0]*n
+ans=1e9
+for i in range(n):
+    g.append(list(map(int, input().split())))
+for i in range(n):
+    f(i, i, 0, 0)
+print(ans)
