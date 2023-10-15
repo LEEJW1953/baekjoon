f3d9a7ae5c431be39605931c40176fdbff3226e8
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-10-15 23:00:30 +0900

    [Gold V] Title: 행성 탐사, Time: 2332 ms, Memory: 236336 KB -BaekjoonHub

--- 백준/Gold/5549. 행성 탐사/행성 탐사.py
+++ 백준/Gold/5549. 행성 탐사/행성 탐사.py
@@ -1,29 +1,27 @@
 import sys
 input=sys.stdin.readline
-
-def count(tmp):
-    if g[i-1][j-1]=='J':
-        tmp[0]+=1
-    if g[i-1][j-1]=='O':
-        tmp[1]+=1
-    if g[i-1][j-1]=='I':
-        tmp[2]+=1
-    return tmp
-
-m, n = map(int, input().split())
+def count(t):
+    if g[i][j]=='J':
+        t[0]+=1
+    if g[i][j]=='O':
+        t[1]+=1
+    if g[i][j]=='I':
+        t[2]+=1
+    return t
+m,n=map(int, input().split())
 k=int(input())
 g=[list(input().strip()) for _ in range(m)]
-arr=[[[0, 0, 0] for _ in range(n+1)]]
-for i in range(1, m+1):
-    tmp=[[0, 0, 0]]
-    for j in range(1, n+1):
-        cur=count(tmp[j-1][:])
-        cur[0]+=arr[i-1][j][0]-arr[i-1][j-1][0]
-        cur[1]+=arr[i-1][j][1]-arr[i-1][j-1][1]
-        cur[2]+=arr[i-1][j][2]-arr[i-1][j-1][2]
-        tmp.append(cur)
-    arr.append(tmp)
+s=[[[0,0,0] for _ in range(n+1)]]
+for i in range(m):
+    t=[[0,0,0]]
+    for j in range(n):
+        c=count(t[j][:])
+        c[0]+=s[i][j+1][0]-s[i][j][0]
+        c[1]+=s[i][j+1][1]-s[i][j][1]
+        c[2]+=s[i][j+1][2]-s[i][j][2]
+        t.append(c)
+    s.append(t)
 for _ in range(k):
     a,b,c,d=map(int, input().split())
-    [j, o, i]=[arr[c][d][i]-arr[c][b-1][i]-arr[a-1][d][i]+arr[a-1][b-1][i] for i in range(3)]
-    print(j, o, i)
+    [j,o,i]=[s[c][d][i]-s[c][b-1][i]-s[a-1][d][i]+s[a-1][b-1][i] for i in range(3)]
+    print(j,o,i)
