eabffa2e695fbe1702e29a88233104c3cfe0f0b7
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-10-17 04:44:07 +0900

    [Bronze III] Title: 공 넣기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,10 @@
+import sys
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+arr=[0]*n
+for _ in range(m):
+    i,j,k=map(int,input().split())
+    for l in range(i-1, j):
+        arr[l]=k
+print(*arr)
