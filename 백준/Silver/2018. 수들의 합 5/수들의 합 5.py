62c95b340caa1f1970c3ecbbd0c02983f4656ef7
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-10-24 23:02:53 +0900

    [Silver V] Title: 수들의 합 5, Time: 632 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
@@ -8,11 +8,12 @@ while st<=n:
     tmp=0
     for i in range(st, en):
         tmp+=i
+        if tmp>n:
+            st+=1
+            break
     if tmp==n:
         ans+=1
         st+=1
     elif tmp<n:
         en+=1
-    else:
-        st+=1
 print(ans)
