d3432809ad131afc87889d6dc197c155ddbdc454
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-11-13 05:22:57 +0900

    [Silver V] Title: 카드1, Time: 64 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -0,0 +1,10 @@
+import sys
+input=sys.stdin.readline
+from collections import deque
+
+n=int(input())
+q=deque(range(1, n+1))
+while q:
+    print(q.popleft(), end=' ')
+    if q:
+        q.append(q.popleft())
