87d0c16d96033313733abd9bbf516aad0f46db60
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-11-27 23:26:10 +0900

    [Silver IV] Title: 국영수, Time: 484 ms, Memory: 83644 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,8 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+arr=[list(map(str, input().split())) for _ in range(n)]
+arr.sort(key=lambda x:(-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+for i in arr:
+    print(i[0])
