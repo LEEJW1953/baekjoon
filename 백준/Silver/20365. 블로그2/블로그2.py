58530147afa8ad4b48b1176a3bd4f73db60e2fcb
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2023-12-24 06:19:05 +0900

    [Silver III] Title: 블로그2, Time: 148 ms, Memory: 32100 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.py
+++ 백준/Silver/20365. 블로그2/블로그2.py
@@ -1,12 +1,17 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
 n=int(input())
-s=input()
-colors = { 'B' : 0, 'R' : 0 }
-colors[s[0]] +=1
+s=input().strip()
+r, b = 0, 0
+if s[0]=='R':
+    r+=1
+else:
+    b+=1
 for i in range(1, n):
-    if s[i] != s[i-1]:
-        colors[s[i]]+=1
-result = min(colors['B'], colors['R'])+1
-print(result)
+    if s[i]!=s[i-1]:
+        if s[i]=='R':
+            r+=1
+        else:
+            b+=1
+print(min(r, b)+1)
