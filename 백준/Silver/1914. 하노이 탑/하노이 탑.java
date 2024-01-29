6d006990c397167c749e8b0bb258065ccf0a498e
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2024-01-29 17:01:14 +0900

    [Silver I] Title: 하노이 탑, Time: 420 ms, Memory: 98740 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.java
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.java
@@ -7,25 +7,27 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static int n;
 	static BigInteger count = new BigInteger("2");
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws Exception {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		count = count.pow(n).subtract(new BigInteger("1"));
-		System.out.println(count);
+		sb.append(count).append("\n");
 
 		if (n <= 20) {
 			hanoi(n, 1, 3);
 		}
+		System.out.println(sb);
 	}
 
 	static void hanoi(int n, int start, int end) {
 		if (n == 1) {
-			System.out.println(start + " " + end);
+			sb.append(start + " " + end).append("\n");
 			return;
 		}
 		hanoi(n - 1, start, 6 - start - end);
-		System.out.println(start + " " + end);
+		sb.append(start + " " + end).append("\n");
 		hanoi(n - 1, 6 - start - end, end);
 	}
 }
