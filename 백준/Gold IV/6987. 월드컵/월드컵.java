e322f2c8a0b1db3dc54371f17282625a3eddd652
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2024-02-15 06:23:53 +0900

    [Gold IV] Title: 월드컵, Time: 72 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Gold IV/6987. 월드컵/월드컵.java
+++ 백준/Gold IV/6987. 월드컵/월드컵.java
@@ -1,86 +1,72 @@
-/**
- * @author 이지원
- * @date 24.02.14
- * @link https://www.acmicpc.net/problem/6987
- * @keyword_solution 6팀의 조별리그는 총 15경기 필요
- * 모든 경기의 모든 결과(승, 무, 패)를 확인하여 유효성 판단
- * @input 4가지 경기 결과
- * @output 가능한 결과는 1, 불가능한 결과는 0 출력
- * @time_complex
- * @perf
- */
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    static int idx = 0, res[][] = new int[6][3], game[][] = new int[15][2];
+   static int[][] match; // 0: 승, 1: 무, 2: 패 정보 입력 받을 배열
+   static boolean flag;
 
-    public static void main(String[] args) throws IOException {
-        // 전체 경기 경우의 수
-        for (int i = 0; i < 6; i++) {
-            for (int j = i + 1; j < 6; j++) {
-                game[idx][0] = i;
-                game[idx++][1] = j;
-            }
-        }
+   public static void main(String[] args) throws IOException {
 
-        for (int i = 0; i < 4; i++) {
-            st = new StringTokenizer(br.readLine());
-            int total = 0;
-            for (int j = 0; j < 6; j++) {
-                for (int k = 0; k < 3; k++) {
-                    int num = Integer.parseInt(st.nextToken());
-                    res[j][k] = num;
-                    total += num;
-                }
-            }
-            // 총 15경기이므로 전체 결과의 합은 30
-            if (total != 30) {
-                sb.append(0 + " ");
-                continue;
-            }
-            sb.append(dfs(0) ? 1 : 0).append(" ");
-        }
-        System.out.println(sb);
-    }
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringBuilder sb = new StringBuilder();
+      // 3개의 경기 결과 주어짐
+      for (int m = 0; m < 4; m++) {
+         match = new int[6][3];
+         int sum = 0;
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         for (int i = 0; i < 6; i++) { // 6개팀 경기 결과 입력
+            sum += match[i][0] = Integer.parseInt(st.nextToken());
+            sum += match[i][1] = Integer.parseInt(st.nextToken());
+            sum += match[i][2] = Integer.parseInt(st.nextToken());
+         }
 
-    static boolean dfs(int d) {
-        if (d == 15) {
-            return true;
-        }
-        // 이번 경기를 하는 두 팀
-        int team1 = game[d][0];
-        int team2 = game[d][1];
-        // 1팀 승리
-        if (0 <= --res[team1][0] & 0 <= --res[team2][2]) {
-            if (dfs(d + 1)) {
-                return true;
-            }
-        }
-        res[team1][0]++;
-        res[team2][2]++;
-        // 무승부
-        if (0 <= --res[team1][1] & 0 <= --res[team2][1]) {
-            if (dfs(d + 1)) {
-                return true;
-            }
-        }
-        res[team1][1]++;
-        res[team2][1]++;
-        // 2팀 승리
-        if (0 <= --res[team1][2] & 0 <= --res[team2][0]) {
-            if (dfs(d + 1)) {
-                return true;
-            }
-        }
-        res[team1][2]++;
-        res[team2][0]++;
-        return false;
-    }
+         if (sum != 30) {// 총 15경기, 점수는 30점이 되어야 하는데 30점이 안되면 무조건 성립되지 않음.
+            sb.append("0 ");
+            continue;
+         }
+
+         flag = false;
+         dfs(0,0,1);
+         if (flag)
+            sb.append("1 ");
+         else
+            sb.append("0 ");
+
+      }
+      System.out.println(sb);
+   }
+
+
+   static void dfs(int count, int t1, int t2) {
+      if (flag)
+         return; // 다른 dfs에서 조건을 만족했으면, 더이상 다른 탐색 진행할 필요 없으므로 return
+      // 가지치기 안해도 O(3^15)으로 시간안에 들어옴
+      if (count == 15) { // 경기 횟수가 15번이 됐으면, 경기가 성립된 것
+         flag = true;
+         return;
+      }
+      // 1. 경기 할 팀 2개 셋팅
+      int nt1 = t1;
+      int nt2 = t2 + 1;
+      if (t2 == 5) {
+         nt1 = t1 + 1;
+         nt2 = nt1 + 1;
+      }
+      // 2. 3가지 경우에 다른 경기 해보기
+      for (int m = 0; m < 3; m++) {// 승-패, 무-무, 패-승 경우 dfs 타기
+         // 3. 정해진 경기 가능한지 받은 결과표에서 확인해보기
+         if (match[t1][m] > 0 && match[t2][2 - m] > 0) {
+            match[t1][m]--;
+            match[t2][2 - m]--;
+            // 다음 경기하러 가기
+            dfs(count + 1, nt1, nt2);
+            match[t1][m]++;
+            match[t2][2 - m]++;
+         }
+      }
+   }
+
 }
