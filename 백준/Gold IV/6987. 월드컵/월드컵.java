143d37fcdc5a21ec1d60121da7c464b68837a333
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2024-02-15 06:21:55 +0900

    [Gold IV] Title: 월드컵, Time: 72 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Gold IV/6987. 월드컵/월드컵.java
+++ 백준/Gold IV/6987. 월드컵/월드컵.java
@@ -13,7 +13,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -31,7 +30,6 @@ public class Main {
             }
         }
 
-        loop:
         for (int i = 0; i < 4; i++) {
             st = new StringTokenizer(br.readLine());
             int total = 0;
