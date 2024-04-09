c327af201b506c5f4e9f777c858e0395aafcfcf5
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2024-04-09 17:20:51 +0900

    [Gold IV] Title: 여왕벌, Time: 1768 ms, Memory: 305384 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.java
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -6,13 +6,15 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
-	static int m, n, g[][];
+	static int m, n, g[][], count, arr[];
 
 	public static void main(String[] args) throws Exception {
 		st = new StringTokenizer(br.readLine());
 		m = Integer.parseInt(st.nextToken());
 		n = Integer.parseInt(st.nextToken());
 		g = new int[m][m];
+		count = 2 * m - 1;
+		arr = new int[count];
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < m; j++) {
 				g[i][j] = 1;
@@ -25,6 +27,23 @@ public class Main {
 			int c = Integer.parseInt(st.nextToken());
 			solve(a, b, c);
 		}
+
+		int x = m - 1, y = 0;
+		for (int i = 0; i < count; i++) {
+			g[x][y] += arr[i];
+			if (x > 0) {
+				x--;
+			} else {
+				y++;
+			}
+		}
+
+		for (int i = 1; i < m; i++) {
+			for (int j = m; j < count; j++) {
+				g[i][j - m + 1] += arr[j];
+			}
+		}
+
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < m; j++) {
 				sb.append(g[i][j]).append(" ");
@@ -35,37 +54,15 @@ public class Main {
 	}
 
 	static void solve(int a, int b, int c) {
-		int count = 2 * m - 1;
-		int[] arr = new int[count];
-		for (int i = 0; i < count; i++) {
-			if (a > 0) {
-				arr[i] = 0;
-				a--;
-			} else if (b > 0) {
-				arr[i] = 1;
-				b--;
-			} else if (c > 0) {
-				arr[i] = 2;
-				c--;
-			}
+		int idx = 0;
+		for (int i = 0; i < a; i++) {
+			arr[idx++] += 0;
 		}
-		int x = m - 1, y = 0;
-		int tmp[][] = new int[m][m];
-		for (int i = 0; i < count; i++) {
-			g[x][y] += arr[i];
-			tmp[x][y] = arr[i];
-			if (x > 0) {
-				x--;
-			} else {
-				y++;
-			}
+		for (int i = 0; i < b; i++) {
+			arr[idx++] += 1;
 		}
-		for (int i = 1; i < m; i++) {
-			for (int j = 1; j < m; j++) {
-				int num = Math.max(tmp[i - 1][j - 1], Math.max(tmp[i][j - 1], tmp[i - 1][j]));
-				tmp[i][j] = num;
-				g[i][j] += num;
-			}
+		for (int i = 0; i < c; i++) {
+			arr[idx++] += 2;
 		}
 	}
 }
