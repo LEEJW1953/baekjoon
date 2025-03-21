65b4258f612106415d76bd0f65364f33bf498d36
LEEJW1953 <110762136+LEEJW1953@users.noreply.github.com>
2025-03-22 05:21:31 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 192 ms, Memory: 19548 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,69 +1,63 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	static int T, N, startX, startY, endX, endY;
-	static boolean[][] dp;
-	static List<Node> list;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int T, N, x, y;
+    static List<Node> list;
+    static boolean[][] vis;
 
-	public static void main(String[] args) throws Exception {
-		T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
-			N = Integer.parseInt(br.readLine());
-			dp = new boolean[N + 2][N + 2];
-			list = new ArrayList<>();
-			for (int i = 0; i < N + 2; i++) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				list.add(new Node(x, y));
-			}
-			for (int i = 0; i < N + 2; i++) {
-				for (int j = 0; j < N + 2; j++) {
-					if (i == j) {
-						dp[i][j] = false;
-					} else {
-						dp[i][j] = list.get(i).compareTo(list.get(j)) > 0;
-					}
-				}
-			}
-			for (int k = 0; k < N + 2; k++) {
-				for (int i = 0; i < N + 2; i++) {
-					for (int j = 0; j < N + 2; j++) {
-						if (!dp[i][j]) {
-							dp[i][j] = dp[i][k] && dp[k][j];
-						}
-					}
-				}
-			}
-			if (dp[0][N + 1]) {
-				sb.append("happy").append("\n");
-			} else {
-				sb.append("sad").append("\n");
-			}
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            N = Integer.parseInt(br.readLine());
+            list = new ArrayList<>();
+            vis = new boolean[N + 2][N + 2];
+            for (int i = 0; i < N + 2; i++) {
+                st = new StringTokenizer(br.readLine());
+                x = Integer.parseInt(st.nextToken());
+                y = Integer.parseInt(st.nextToken());
+                list.add(new Node(x, y));
+            }
+            for (int i = 0; i < N + 2; i++) {
+                for (int j = 0; j < N + 2; j++) {
+                    if (i != j) {
+                        vis[i][j] = list.get(i).compareTo(list.get(j)) > 0;
+                    }
+                }
+            }
+            for (int k = 0; k < N + 2; k++) {
+                for (int i = 0; i < N + 2; i++) {
+                    for (int j = 0; j < N + 2; j++) {
+                        if (!vis[i][j]) {
+                            vis[i][j] = vis[i][k] && vis[k][j];
+                        }
+                    }
+                }
+            }
+            if (vis[0][N + 1]) {
+                sb.append("happy").append("\n");
+            } else {
+                sb.append("sad").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 
-	static class Node implements Comparable<Node> {
-		int x;
-		int y;
+    static class Node implements Comparable<Node> {
+        int x, y;
 
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
 
-		@Override
-		public int compareTo(Node o) {
-			int dist = Math.abs(this.x - o.x) + Math.abs(this.y - o.y);
-			return dist <= 1000 ? 1 : -1;
-		}
-	}
+        @Override
+        public int compareTo(Node O) {
+            int dist = Math.abs(this.x - O.x) + Math.abs(this.y - O.y);
+            return dist <= 1000 ? 1 : -1;
+        }
+    }
 }
